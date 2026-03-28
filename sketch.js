47faed3d6afd0a6192b060ab5f28527b3f44c6c8
LauraRad <30a18@kjg.lt>
2026-03-28 10:20:41 +0200

    circle going in circle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(400, 400);
 
-  frameRate(1)
+  frameRate(5)
 }
 
 function draw() {
@@ -10,4 +10,14 @@ function draw() {
     Math.floor(Math.random() * 255),
     Math.floor(Math.random() * 255)
   );
+
+  var circle_path_radius = 100;
+  var circle_path_center_x = 200;
+  var circle_path_center_y = 200;
+  var time = millis() / 1000;
+
+  var circle_center_x = circle_path_radius*cos(millis() / 1000) + circle_path_center_x;
+  var circle_center_y = circle_path_radius*sin(millis() / 100) + circle_path_center_y;
+
+  circle(circle_center_x, circle_center_y, 100)
 }
