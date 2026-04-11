d40a38e3c296d5469355bce0af3eb995d2459595
LauraRad <30a18@kjg.lt>
2026-04-11 11:23:28 +0300

    Bouncables :P

--- sketch.js
+++ sketch.js
@@ -1,25 +1,14 @@
 var bouncables = [];
 
+var setupScript = function() {};
+var drawScript = function() {};
+
 function setup() {
   createCanvas(400, 400);
-
-  for (var i = 0; i < 100; i++) {
-    bouncables.push(new Bouncable(
-      color(random(0, 255), random(0, 255), random(0, 255)),
-      random(50, 350), 
-      random(50, 350), 
-      random(5, 50), 
-      random(1, 5), 
-      random(0, 360)
-      
-    ));
-  }
 }
 
 function draw() {
   background(255);
 
-  for (var i = 0; i < bouncables.length; i++) {
-    bouncables[i].draw();
-  }
+  drawScript();
 }
