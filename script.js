2ba5ecf5371408bc4bc7e686a06a38c42c087a06
LauraRad <30a18@kjg.lt>
2026-04-18 10:45:48 +0300

    Created snake

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ function projectSelected() {
     if (projectsSelect.value === 'bouncables') {
         setupScript = setupBouncables;
         drawScript = drawBouncables; 
-    
     } else if (projectsSelect.value === 'flags') {
         setupScript = function() {};
         drawScript = drawFlags;
+    } else if (projectsSelect.value === 'snake') {
+        setupScript = setupSnake;
+        drawScript = drawSnake;
     }
+
+
+
+
 
     setupScript();
 }
