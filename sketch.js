9c0fc157f5bd8232d9a2c4e92e881f302b4f6cb6
LauraRad <30a18@kjg.lt>
2026-04-20 07:47:03 +0300

    Snake image

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 var setupScript = function() {};
 var drawScript = function() {};
 
+var canvasX = 400;
+var canvasY = 400;
+
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
+  background(220);
 
   drawScript();
 }
